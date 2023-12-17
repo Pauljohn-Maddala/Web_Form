cb3dc3524fa56a09c5ea15e2abfded8c63970760
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-17 12:47:05 -0500

    Update web_users.py

--- web_users.py
+++ web_users.py
@@ -31,7 +31,6 @@ class MemberManager:
             nickname = f"{original_nickname}_{nickname_suffix}"
             nickname_suffix += 1
 
-        raise ValueError("Nickname already in use")
 
         self.id_counter += 1
         member_access_key = os.urandom(24).hex()
