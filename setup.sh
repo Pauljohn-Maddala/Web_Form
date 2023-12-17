02c342effe1880c11cc9ea73e23266e3067bf7d5
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 22:05:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,31 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-# Install and use the latest Node.js version
+# Install and use the latest Node.js version#!/bin/sh
+
+# Install nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Load nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install the latest version of Node.js
+nvm install node
+nvm use node
+nvm alias default node
+
+# Check if Newman is installed and remove it if it is
+if npm list -g | grep -q newman; then
+  npm uninstall -g newman
+fi
+
+# Install Newman
+npm install -g newman
+
+# Display the Node.js version
+echo "Using Node.js version: $(node -v)"
+
 nvm install node
 nvm use node
 nvm alias default node
