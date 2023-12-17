218dd49f82df4cd6971f5cba78674b96026aec5b
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 22:23:57 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,20 +7,19 @@ pip3 install Flask pytest
 if ! command -v nvm &> /dev/null; then
     # Install nvm (Node Version Manager)
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    # Source nvm in current shell
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 fi
 
-# Source nvm in current shell
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Check if nvm is sourced successfully
+# Check if nvm is sourced correctly
 if [ -z "$NVM_DIR" ]; then
     echo "Error: nvm not sourced correctly"
     exit 1
 fi
 
 # Install the required Node.js version
-nvm install node
+nvm install 16
 
 # Check if Node.js is installed successfully
 if ! command -v node &> /dev/null; then
