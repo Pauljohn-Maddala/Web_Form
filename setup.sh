952604e8d17ed1d344593a989cbdae9e5cb1e6ce
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 22:16:58 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
-# Assuming nvm is installed, use it to install and use Node.js v16
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install 16
-nvm use 16
-
 # Check the installation path and permissions for Newman
 if [ -d "/usr/local/lib/node_modules/newman" ]; then
     sudo rm -rf /usr/local/lib/node_modules/newman
 fi
 
 # Install Newman
-npm install -g newman
+npm update -g newman
+
+# Check the installation path and permissions for Newman
+if [ -d "/usr/local/lib/node_modules/newman" ]; then
+    sudo rm -rf /usr/local/lib/node_modules/newman
+fi
+
+# Assuming nvm is installed, use it to install and use Node.js v16
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 16
+nvm use 16
+
+
+
+
 
 # Display Node.js version
 echo "Using Node.js version: $(node -v)"
