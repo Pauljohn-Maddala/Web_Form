e8859fac1fb5ec7fb0f30f986ef1aa312a02cb50
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-17 01:17:48 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,59 +1,75 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from marshmallow import Schema, fields, validate
+import logging
 import secrets
-from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SECRET_KEY'] = 'your-secret-key'
 
-posts = {}
-next_id = 1
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+limiter = Limiter(app, key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
+
+logging.basicConfig(filename='server.log', level=logging.DEBUG)
+
+# User model for authentication
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # Add additional user fields as necessary
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Post model for database
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), nullable=False)
+
+# Schema for data validation
+class PostSchema(Schema):
+    content = fields.Str(required=True, validate=validate.Length(max=500))
+
+post_schema = PostSchema()
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Implement your login logic here
+    pass
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'Logged out'
 
 @app.route('/post', methods=['POST'])
+@limiter.limit("10 per minute")
 def create_post():
-    global next_id
+    app.logger.info('Post request received')
     if not request.is_json:
         return "Invalid format, JSON required", 400
 
     data = request.get_json()
-    if 'msg' not in data or not isinstance(data['msg'], str):
-        return "Bad request: 'msg' field missing or not a string", 400
+    errors = post_schema.validate(data)
+    if errors:
+        return jsonify(errors), 400
 
-    key = secrets.token_urlsafe(16)
-    timestamp = datetime.utcnow().isoformat() + "Z"
-    posts[next_id] = {'msg': data['msg'], 'key': key, 'timestamp': timestamp}
-    
-    response = {'id': next_id, 'key': key, 'timestamp': timestamp}
-    next_id += 1
-    return jsonify(response)
+    post = Post(content=data['content'])
+    db.session.add(post)
+    db.session.commit()
 
-
-
-@app.route('/post/<int:post_id>', methods=['GET'])
-def read_post(post_id):
-    post = posts.get(post_id)
-    if post is None:
-        return "Post not found", 404
-
-    response = {
-        'id': post_id,
-        'timestamp': post['timestamp'],
-        'msg': post['msg']
-    }
-    return jsonify(response)
-
-
-@app.route('/post/<int:post_id>/delete/<key>', methods=['DELETE'])
-def delete_post(post_id, key):
-    post = posts.get(post_id)
-    if post is None:
-        return "Post not found", 404
-
-    if post['key'] != key:
-        return "Forbidden: Incorrect key", 403
-
-    del posts[post_id]
-    return jsonify({'id': post_id, 'key': key, 'timestamp': post['timestamp']})
-
-
+    return jsonify({'message': 'Post created', 'id': post.id}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
