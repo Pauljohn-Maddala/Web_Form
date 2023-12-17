2f9f284b5e57bfa3ac76ae94bedc25bf14b07d4e
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 23:20:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# Install Node.js 16.x using Node Version Manager (NVM)
+# NVM allows you to manage multiple Node.js versions
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.nvm/nvm.sh  # Load NVM
+nvm install 16       # Install Node.js 16.x
+nvm use 16            # Use Node.js 16.x as the active version
 
-# Install Newman globally
+# Install Newman globally using npm
 npm install -g newman
 
-# Check Newman version
-newman --version
+# Install other npm packages if needed
+# npm install package-name
 
-# Additional setup steps as needed
+# Print a message indicating that setup is complete
+echo "Setup is complete."
