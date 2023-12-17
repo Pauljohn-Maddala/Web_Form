57b8187c96aefd94414bdfd5001d3bd4b34bc38f
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 20:55:30 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,11 +4,8 @@ set -e # exit immediately if newman complains
 trap 'kill $PID' EXIT # kill the server on exit
 
 ./run.sh &
-PID=$! # record the PID of the server
+PID=$! # record the PID
 
-# Test creating multiple posts
-newman run forum_multiple_posts.postman_collection.json -e env.json
-
-# Test reading and deleting posts, 50 iterations
-newman run forum_post_read_delete.postman_collection.json -n 50
+newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
+newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
 
