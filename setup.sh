48b1bb706194dc5131ae336584b385224a0a6005
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 22:56:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
 
+# Install Python dependencies
+pip3 install Flask pytest
 
-sudo rm -rf /usr/local/lib/node_modules/newman
+# Check if NVM_DIR is defined
+if [ -z "$NVM_DIR" ]; then
+    export NVM_DIR="$HOME/.nvm"
+fi
 
+# Check if nvm.sh exists
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    # Source nvm in current shell
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+    # Install the latest version of Node.js
+    nvm install --lts
+
+    # Use the latest LTS version
+    nvm use --lts
+else
+    echo "NVM is not installed. Please install NVM to manage Node.js versions."
+fi
+
+# Install Newman globally
 npm install -g newman
+
+# Check Newman version
+newman --version
+
+# Additional setup steps as needed
 
