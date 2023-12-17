697d0c8578ecd9daf10cb05d4bfec0a7b296f0ec
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 23:12:26 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,4 +9,4 @@ PID=$! # record the PID
 #newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
 #newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
 
-newman run sample_test.json
+newman run sample_test.json -e env.json
