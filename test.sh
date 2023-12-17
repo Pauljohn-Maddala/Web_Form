35187f51e1ef614785bf8e0b63bc5935063b600b
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 20:43:57 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,14 @@
-#!/bin/bash
-pytest  
+#!/bin/sh
+
+set -e # exit immediately if newman complains
+trap 'kill $PID' EXIT # kill the server on exit
+
+./run.sh &
+PID=$! # record the PID of the server
+
+# Test creating multiple posts
+newman run forum_multiple_posts.postman_collection.json -e env.json
+
+# Test reading and deleting posts, 50 iterations
+newman run forum_post_read_delete.postman_collection.json -n 50
+
