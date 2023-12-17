1d901a0c50955b5e8eb273f8066c969dda1458aa
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 21:01:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-pip install -r requirements.txt  # assuming you have a requirements.txt file
+#!/bin/sh
+
+# Install Python dependencies
+pip3 install Flask pytest
+
+# Install nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Source nvm in current shell
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install Node.js version 16
+nvm install 16
+nvm use 16
