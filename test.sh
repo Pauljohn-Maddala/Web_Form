32321fd2c0b3efc3fd11432a5a696ea80185a9ce
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 23:22:58 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# Use Node.js 16.x from NVM
+source ~/.nvm/nvm.sh  # Load NVM
+nvm use 16            # Use Node.js 16.x as the active version
+
 set -e # exit immediately if newman complains
 trap 'kill $PID' EXIT # kill the server on exit
 
 ./run.sh &
 PID=$! # record the PID
 
-#newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
-#newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
-
-newman run sample_test.json -e env.json
+newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
+newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
