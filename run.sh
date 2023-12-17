bca6cbb41cadf4064ac9f3e5c3b1e2df18be72ed
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-17 15:39:07 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export FLASK_APP=server.py  # replace with your Flask app file name
+export FLASK_APP=server_v2.py  # replace with your Flask app file name
 export FLASK_ENV=development
 exec flask run
