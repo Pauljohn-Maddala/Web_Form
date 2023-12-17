320f9c8f49b966272b3c3cd47bf98d7897c47f63
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-17 15:49:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+pip3 install Flask flask-sqlalchemy flask-login flask-limiter marshmallow werkzeug
+
+# Install or update NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Source NVM in the current shell
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+
+# Install Node.js 16.x
+nvm install 16
+
+# Use Node.js 16.x
+nvm use 16
+
+# Install Newman
+npm install -g newman
+
 # Install OpenSSL
 pip3 install pyopenssl
 
