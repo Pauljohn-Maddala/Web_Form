f3ee5daa776a84057d1a9e6daa3765c9f75de85f
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 23:03:55 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
-# Create a virtual environment for Python
-python3 -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Install Python dependencies
-pip install Flask pytest
-
 # Install Newman globally
 npm install -g newman
 
 # Check Newman version
 newman --version
+
+# Additional setup steps as needed
