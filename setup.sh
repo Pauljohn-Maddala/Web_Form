bc58952ae0ee859492ae4bd9bc161c91795b488a
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 21:04:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Install Node.js version 16
-nvm install 16
-nvm use 16
+# Install the latest version of Node.js
+nvm install node
+nvm use node
