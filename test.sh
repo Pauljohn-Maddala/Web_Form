6958601d211105362f3fd2ce26b4b83bbcf34fb3
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 23:26:10 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Use Node.js 16.x from NVM
-source ~/.nvm/nvm.sh  # Load NVM
-nvm use 16            # Use Node.js 16.x as the active version
+# Source NVM in the current shell
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
 
 set -e # exit immediately if newman complains
 trap 'kill $PID' EXIT # kill the server on exit
@@ -10,5 +10,15 @@ trap 'kill $PID' EXIT # kill the server on exit
 ./run.sh &
 PID=$! # record the PID
 
+# Use the correct Node.js version with NVM
+nvm use 16
+
+# Run Newman tests
 newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
 newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
+
+# Additional test commands (if needed)
+# ...
+
+# Exit
+exit 0
