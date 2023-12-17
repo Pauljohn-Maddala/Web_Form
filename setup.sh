372c5a2d1a5c4ade4f3619dc1e6a43186f49b5a6
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-16 21:41:16 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# Install Python dependencies
-pip3 install Flask pytest
-
 # Install nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-npm install -g newman
-
-# Source nvm in current shell
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# Load nvm
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Install the latest version of Node.js
 nvm install node
+
+# Use the installed Node.js version
 nvm use node
+
+# Install or upgrade Newman
+npm install -g newman
