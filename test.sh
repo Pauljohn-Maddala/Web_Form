5031c4ba711f0d8e6f5cc0fe1482a309f4a2152b
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-12-17 21:26:41 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ python3 test.py
 newman run forum_multiple_posts.postman_collection.json -e env.json
 newman run forum_post_read_delete.postman_collection.json -e env.json
 
-kill APP_PID=$!
+kill $APP_PID
 
 echo "Tests completed."
 
